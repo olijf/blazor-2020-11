ea03edfbbed7c520fbeae151ded3fdcd69fa6820
JP <jp@jp.nl>
2020-11-02 10:41:41 +0100

    Hello world in Blazor

---
+++ DemoProject/DemoProject/DemoProject/Models/FrameworkModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProject.Models
+{
+	public class FrameworkModel
+	{
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+
+		public byte Grade { get; set; }
+
+		public string Logo { get; set; }
+	}
+}
---
+++ DemoProject/DemoProject/DemoProject/Pages/HelloWorld.razor.cs
@@ -0,0 +1,36 @@
+﻿using DemoProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoProject.Pages
+{
+	public partial class HelloWorld
+	{
+		public string Title { get; set; } = "Nogmaals hallo wereld";
+
+		public List<FrameworkModel> Frameworks { get; set; } = new List<FrameworkModel>()
+		{
+			new FrameworkModel() { Id = 4, Name = "AngularJS", Grade = 3, Logo = "" },
+			new FrameworkModel() { Id = 8, Name = "Angular", Grade = 8, Logo = ""},
+			new FrameworkModel() { Id = 8, Name = "Blazor", Grade = 7, Logo = ""},
+			new FrameworkModel() { Id = 15, Name = "Vue", Grade = 8, Logo = ""}
+		};
+
+		public void ChangeTitle()
+		{
+			Title += "Doei wereld";
+		}
+
+		public void AddFramework()
+		{
+			Frameworks.Add(new FrameworkModel()
+			{
+				Id = 23,
+				Name = "React",
+				Grade = 2,
+				Logo = ""
+			});
+		}
+	}
+}
